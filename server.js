f313f969741d85c23e839a2afe06b3d911145ad1
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-30 21:30:00 +0530

    server.js

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
-const imports = require('./data');
+const http = require('http');
 
-console.log(imports);
+const server = http.createServer((req, res) => {
+
+    console.log("Request received");
+    console.log("URL:", req.url);
+    console.log("Method:", req.method);
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    // Routing
+    if (req.url === "/") {
+        res.write("Welcome to Home Page");
+        res.end();
+    }
+
+    else if (req.url === "/about") {
+        res.write("Welcome to About Page");
+        res.end();
+    }
+
+    else if (req.url === "/contact") {
+        res.write("Welcome to Contact Page");
+        res.end();
+    }
+
+    else {
+        res.write("404 Page Not Found");
+        res.end();
+    }
+
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log("Server running at http://localhost:3000");
+});
